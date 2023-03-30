87d57ec4893b4b37859c93ef3c327b3e4921af50
yuanliping <yuanliping@tenorshare.cn>
2023-03-30 12:04:16 +0800

    rename and 组件

---
+++ code/14.组件/单文件组件/School.vue
@@ -0,0 +1,26 @@
+<!-- 组件的结构 -->
+<template>
+    <div>
+        <h3>学校名称：{{ name }}</h3>
+        <h3>学校地址：{{ address }}</h3>
+        <button @click="showName">tips</button>
+    </div>
+</template>
+
+<script>
+// 交互相关的代码（数据、方法等）
+export default {
+    name: 'School',
+    data() {
+        return {
+            name: "深圳大学",
+            address: "深圳"
+        }
+    },
+    methods: {
+        showName() {
+            console.log(this.name);
+        }
+    },
+}
+</script>
---
+++ code/14.组件/单文件组件/Student.vue
@@ -0,0 +1,32 @@
+<!-- 组件的结构 -->
+<template>
+    <div class="demo">
+        <h3>学生名称：{{ name }}</h3>
+        <h3>学生年龄：{{ age }}</h3>
+        <button @click="showName">tips</button>
+    </div>
+</template>
+
+<script>
+// 交互相关的代码（数据、方法等）
+export default {
+    name: 'Student',
+    data() {
+        return {
+            name: "张三",
+            age: "18"
+        }
+    },
+    methods: {
+        showName() {
+            console.log(this.name);
+        }
+    },
+}
+</script>
+<style>
+/* 组件的样式 */
+.demo h3 {
+    color: blueviolet;
+}
+</style>
